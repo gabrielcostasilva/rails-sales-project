ba802dd6b0ed190f41756992f302deb630c32270
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2024-09-18 20:33:03 -0300

    Added City

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_15_194715) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_18_232627) do
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.integer "state_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["state_id"], name: "index_cities_on_state_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -23,4 +31,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_15_194715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "cities", "states"
 end
